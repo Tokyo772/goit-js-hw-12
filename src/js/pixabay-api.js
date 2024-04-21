2e571c08856db26a8c39ac2d1da95f65772fb9cd
Tokyo772 <merokyry33@gmail.com>
2024-04-21 21:29:01 +0200

    pixabay done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
-async function serviceImage(userSearch) {
-  const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '43457723-16bfd608d3311c06907a5c683';
 
+async function serviceImage(userSearch, page = 1) {
   const params = new URLSearchParams({
-    key: '43457723-16bfd608d3311c06907a5c683',
+    key: API_KEY,
     q: userSearch,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    page,
+    per_page: 15,
   });
 
   const response = await axios.get(`${BASE_URL}?${params}`);
